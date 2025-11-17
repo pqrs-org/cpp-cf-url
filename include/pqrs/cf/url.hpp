3b8714a0fa44a0e62e8c018c63af44bbededa046
Takayama Fumihiko <tekezo@pqrs.org>
2025-11-18 08:10:07 +0900

    Remove redundant pqrs::cf::

--- include/pqrs/cf/url.hpp
+++ include/pqrs/cf/url.hpp
@@ -13,7 +13,7 @@
 namespace pqrs {
 namespace cf {
 inline std::optional<std::string> make_string(CFURLRef url) {
-  return pqrs::cf::make_string(CFURLGetString(url));
+  return make_string(CFURLGetString(url));
 }
 
 inline cf_ptr<CFURLRef> make_url(const std::string& file_path) {
